b7655ff57e79ddf4c5aee0bf91c743419011f324
Aditya Vijay <avijay@usc.edu>
2014-10-31 17:00:23 -0700

    Create compressCString.c

---
+++ compressCString.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+char *alterString(char *name){
+    char *newName = (char *)malloc(sizeof(char)*strlen(name));
+    newName[strlen(name)] = '\0';
+    
+    int i = 0;
+    int j = 0;
+    int cnt = 0;
+    
+    while(name[i] != '\0'){
+        cnt = 0;
+        newName[j] = name[i];
+        while(name[i] == name[i+1]){
+            i++;
+            cnt++;
+        }
+        if(cnt>0){
+            newName[j+1] = cnt+1 + '0';
+            j = j+2;
+            i++;
+        }else{
+            newName[j+1] = cnt+1+'0';
+            j = j + 2;
+            //j++;
+            i++;
+        }
+    }
+    
+    newName[j] = '\0';
+    return newName;
+}
+
+int main(){
+    char name[100] = "aaabbbdddeefdgdfgnjnnndf";
+    char *newName;
+    newName = alterString(name);
+    printf("%s\n",newName);
+    return 0;
+}
